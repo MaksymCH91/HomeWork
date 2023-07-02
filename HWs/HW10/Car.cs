cf7efc8f2e266cc6b534dce55a6a3824ca74a841
Чорноконь Максим Олександрович <aimmaxblack@gmail.com>
2023-07-02 18:53:26 +0300

    HW10

---
+++ HWs/HW10/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW10
+{
+    public abstract class Car 
+    {
+        public Car(string model) 
+        { 
+            this .model = model;
+        }
+        public string model = "-1";
+        public int MaxSpeed = 50;
+        public string BrakesPedal = "stop";
+        public string AcceleratorPedal = "accelerator pedal";
+        virtual public void trip()
+        {
+            Console.WriteLine(AcceleratorPedal + " " + MaxSpeed + " " + "BrakesPedal");
+        }
+    }
+    public class BMW: Car
+    {
+        public BMW(string model) : base(model)
+        {
+            this.model = model;
+        }
+        public override void trip()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"BMW {model}:" + ( AcceleratorPedal+" Fast speed accelerastion ")+(MaxSpeed + 200)+" " + BrakesPedal + " in 10 sec");
+        }
+
+    }
+    public class Porshe : Car
+    {
+        public Porshe(string model) : base(model)
+        {
+            this.model = model;
+        }
+        public override void trip()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Porshe {model}:" + (AcceleratorPedal + " hight speed ") + (MaxSpeed + 250) + " " + BrakesPedal + "In 5 sec");
+        }
+    }
+}
+ 
---
+++ HWs/HW10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace HW10
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List < Car > Cars = new List<Car>()
+            {
+                new BMW("x5"), new Porshe("Carera GT"),new BMW ("X2")
+            }; 
+       foreach (var Car in Cars)
+            {
+                Car.trip();
+                Console.WriteLine();
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+
+        }
+
+    }
+}
