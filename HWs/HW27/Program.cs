3630cd0f0365bb7100e983615e3f8875af785cc0
Чорноконь Максим Олександрович <aimmaxblack@gmail.com>
2023-09-30 18:19:22 +0300

    HW28

---
+++ HWs/HW27/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace HW27
+{
+    class Program
+    {
+    static async Task Main(string[] args)
+    {
+        int numThreads = Environment.ProcessorCount; // Кількість доступних процесорних ядер за замовчуванням
+
+       
+       
+            numThreads = 4; // Використовуємо задану кількість потоків, якщо користувач вказав параметр
+        
+
+        Console.WriteLine($"Using {numThreads} thread(s) for asynchronous execution.");
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+            for (int i = 1; i < 1000000; i++)
+            {
+                Task<int> task = CalculateAsync(10, numThreads); 
+                int result = await task;
+                Console.WriteLine($"Result: {result}");
+            }
+        
+
+        Console.WriteLine("Task started");
+
+        
+
+        stopwatch.Stop();
+        ;
+        Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds}ms");
+    }
+
+    static async Task<int> CalculateAsync(int number, int numThreads)
+    {
+        return await Task.Run(() => CalculateSquare(number, numThreads));
+    }
+
+    static int CalculateSquare(int number, int numThreads)
+    {
+        Console.WriteLine($"Calculating square of {number} using {numThreads} thread(s).");
+
+        // Демонстраційна операція обчислення квадрату (тут може бути реальний важкий обчислювальний код)
+        int result = number * number;
+
+        return result;
+    }
+}
+}
---
+++ HWs/HW28/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace HW28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+           
+            for (int i = 1; i <= Environment.ProcessorCount; i++)
+            {
+                Console.WriteLine($"numbers of treads: {i}");
+                int[] numbers = Enumerable.Range(1, 100000000).ToArray();
+                var stopwatch = Stopwatch.StartNew();
+
+                var result = numbers.AsParallel()
+                    .WithDegreeOfParallelism(i) // set numbe rs of treads
+                    .Where(x => x % 2 == 0)
+                    .Select(x => x * x)
+                    .ToList();
+
+                stopwatch.Stop();
+
+                Console.WriteLine($"Result: {result.Count}");
+                Console.WriteLine($"Execution time: {stopwatch.ElapsedMilliseconds} ms");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
