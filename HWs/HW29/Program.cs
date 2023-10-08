6ed1e87d99dc69ff35a455292084f4082d5594bc
Чорноконь Максим Олександрович <aimmaxblack@gmail.com>
2023-10-08 21:40:44 +0300

    hw29 done

--- HWs/HW29/Program.cs
+++ HWs/HW29/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace HW29
+﻿using System.Reflection;
+
+namespace HW29
 {
-    internal class Program
+    class MyClass
+    {
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void PrintCurrentTime()
+        {
+            Console.WriteLine($"Current Time: {DateTime.Now.ToString("HH:mm:ss")}");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyClass obj = new MyClass();
+
+            // List all available methods
+            Console.WriteLine("Available methods:");
+            Type myClassType = typeof(MyClass);
+            foreach (MethodInfo methodInfo in myClassType.GetMethods())
+            {
+                Console.WriteLine(methodInfo.Name);
+            }
+
+            // Prompt for method selection
+            Console.Write("Enter the method name to call (Print, PrintCurrentTime, or other): ");
+            string methodName = Console.ReadLine();
+
+            try
+            {
+                // Use reflection to call the selected method
+                MethodInfo methodInfo = myClassType.GetMethod(methodName);
+
+                if (methodInfo != null)
+                {
+                    methodInfo.Invoke(obj, null);
+                }
+                else
+                {
+                    Console.WriteLine("Method not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
     }
 }
